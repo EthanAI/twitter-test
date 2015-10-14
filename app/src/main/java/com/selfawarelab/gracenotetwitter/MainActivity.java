42beb6cc95e435ba6efd1ed73a0256c8d29e7552
Ethan Smith <ethan@selfawarelab.com>
2015-10-14 15:38:45 -0700

    init commit

---
+++ app/src/main/java/com/selfawarelab/gracenotetwitter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.selfawarelab.gracenotetwitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
