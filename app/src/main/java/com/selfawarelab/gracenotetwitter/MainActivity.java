9c83b4b7434b859199bafee014c842a3180ae52b
Ethan Smith <esmith@mobitv.com>
2015-10-18 16:21:11 -0700

    Get tweets

--- app/src/main/java/com/selfawarelab/gracenotetwitter/MainActivity.java
+++ app/src/main/java/com/selfawarelab/gracenotetwitter/MainActivity.java
@@ -3,14 +3,23 @@ package com.selfawarelab.gracenotetwitter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.AppSession;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.StatusesService;
+import com.twitter.sdk.android.tweetui.CompactTweetView;
 import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.TweetViewFetchAdapter;
+
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -44,6 +53,7 @@ explanation.
  */
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "3m2UoJLnYMmNU5K5K4RdRSksu";
@@ -53,25 +63,74 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
 
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Twitter twitter = new Twitter(authConfig);
+        Fabric.with(this, twitter);
+
         getTweets();
+
     }
 
     public void getTweets() {
         // TODO: Base this Tweet ID on some data from elsewhere in your app
-        long tweetId = 631879971628183552L;
-        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+//        long tweetId = 631879971628183552L;
+        long tweetId = 655116638522466304L;
+//        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+//            @Override
+//            public void success(Result<Tweet> result) {
+//                Log.d("TwitterKit", "Success: " + result.data.createdAt + " " + result.data.text);
+//            }
+//
+//            @Override
+//            public void failure(TwitterException exception) {
+//                Log.d("TwitterKit", "Load Tweet failure", exception);
+//            }
+//        });
+
+//        TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+//        StatusesService statusesService = twitterApiClient.getStatusesService();
+//        statusesService.userTimeline(null, "screen_name=twitterapi&count=2", null, null, null, null, null, null, null, new Callback<List<Tweet>>() {
+//            @Override
+//            public void success(Result<List<Tweet>> result) {
+//                List<Tweet> tweets = result.data;
+//                Log.d(TAG, "got tweets: " + tweets);
+//            }
+//
+//            @Override
+//            public void failure(TwitterException e) {
+//                Log.d(TAG, "Failed to get tweets");
+//            }
+//        });
+
+        TwitterCore.getInstance().logInGuest( new Callback<AppSession>() {
             @Override
-            public void success(Result<Tweet> result) {
-                Log.d("TwitterKit", "Success");
+            public void success(Result<AppSession> appSessionResult) {
+                AppSession session = appSessionResult.data;
+                TwitterApiClient twitterApiClient =  TwitterCore.getInstance().getApiClient(session);
+                twitterApiClient.getStatusesService().userTimeline(null, "seinfeldtoday", 10, null, null, false, false, false, true, new Callback<List<Tweet>>() {
+                    @Override
+                    public void success(Result<List<Tweet>> listResult) {
+                        List<Tweet> tweets = listResult.data;
+                        Log.d(TAG, "got tweets: " + tweets);
+                        for(Tweet tweet : tweets) {
+                            Log.d(TAG, tweet.createdAt + " " + tweet.text);
+                        }
+                    }
+
+                    @Override
+                    public void failure(TwitterException e) {
+                        Log.d(TAG, "Could not retrieve tweets");
+                        e.printStackTrace();
+                    }
+                });
             }
 
             @Override
-            public void failure(TwitterException exception) {
-                Log.d("TwitterKit", "Load Tweet failure", exception);
+            public void failure(TwitterException e) {
+                Log.d(TAG, "Could not get guest Twitter session");
+                e.printStackTrace();
             }
         });
     }
